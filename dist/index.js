1a7ff0a05e1af90e9d2d35ba4ce25da1f1baa234
unknown <sabinorotondi2000@gmail.com>
2022-07-04 18:00:09 +0200

    feat: type aliases ex done

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Create a `Country` type alias for the `string` type.
+// Change the type for each variable to use the `Country` type.
+const country1 = "Nigeria";
+const country2 = "Italy";
+const country3 = "China";
+console.log(country1, country2, country3);
