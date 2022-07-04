a934c62462c7cde9026b363ffc0be38044101042
unknown <sabinorotondi2000@gmail.com>
2022-07-04 18:24:01 +0200

    feat define-object ex done

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+const countryPopulation = {
+    code: "NZ",
+    population: 5135300,
+};
+const countryData = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+const currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+};
