a097128741e4160c3c894a1d499a950146f37830
unknown <sabinorotondi2000@gmail.com>
2022-07-05 18:52:13 +0200

    extend interface ex done

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const countryA = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+const countryB = {
+    name: "Italy",
+    code: "IT"
+};
+const countryC = {
+    name: "Spain",
+    code: "ES",
+    population: 47450795,
+};
+console.log(countryA, countryB, countryC);
