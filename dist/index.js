066437a5b7bd6e9544916e9b09e18c98853931ba
unknown <sabinorotondi2000@gmail.com>
2022-07-13 04:10:30 +0200

    feat: unions ex done

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let indianCurrency = "Indian rupee";
+let narniaCurrency = false;
+function outputCountryData(country, data) {
+    console.log(`The data for ${country} is: ${data}`);
+}
+outputCountryData("Italy", 60317116);
+outputCountryData("Nigeria", "Naira");
+outputCountryData("United States of America", "English");
+outputCountryData("India", 1352642280);
