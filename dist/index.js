9c20b9e18b82d80b68b1c2b9efc253543915a2b5
unknown <sabinorotondi2000@gmail.com>
2022-07-13 04:16:49 +0200

    feat: mapped type ex done

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const countryDataDescriptions = {
+    name: "The name of the country.",
+    languages: "The languages spoken in the country.",
+    population: "The population of the country.",
+};
