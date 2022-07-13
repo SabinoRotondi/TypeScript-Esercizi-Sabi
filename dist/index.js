0e0716c57662c40abd60b5d57ef51fcc9d12104a
unknown <sabinorotondi2000@gmail.com>
2022-07-13 04:23:29 +0200

    feat: type with record ex done

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const currency = {
+    name: "United States dollar",
+    code: "USD",
+    symbol: "$",
+};
+const currencies = {
+    france: {
+        name: "Euro",
+        code: "EUR",
+        symbol: "€",
+    },
+    china: {
+        name: "Renminbi",
+        code: "CNY",
+        symbol: "¥",
+    },
+    colombia: {
+        name: "Colombian peso",
+        code: "COP",
+        symbol: "$",
+    }
+};
